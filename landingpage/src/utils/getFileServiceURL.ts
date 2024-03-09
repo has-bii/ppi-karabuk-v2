40280e20d4b5f606db41a21eac8bb9ae20dfd9c1
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-03-09 19:41:05 +0300

    build: delete unused

--- landingpage/src/utils/getFileServiceURL.ts
+++
@@ -1,5 +0,0 @@
-export default function getFileServiceURL(path: string): string {
-  return process.env.NODE_ENV !== "development"
-    ? process.env.NEXT_PUBLIC_IMG_PROVIDER_URL + path
-    : path
-}
--- landingpage/src/utils/style/getSize.ts
+++
@@ -1,5 +0,0 @@
-import { Size } from "@/types/style"
-
-export default function getSize(size: Size, component?: string): string {
-  return `${component ? component + "-" : ""}${size}`
-}
