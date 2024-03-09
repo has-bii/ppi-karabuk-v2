9e8f5d2e6dc1ccfd5bab662d9cfc9b30c12e13a3
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-03-09 16:59:06 +0300

    init blog

---
+++ blog/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-supa-own",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET"),
+        directory: env("SUPABASE_DIRECTORY"),
+      },
+    },
+  },
+});
---
+++ blog/providers/strapi-provider-upload-supa-own/index.js
@@ -0,0 +1,127 @@
+"use strict";
+
+const { createClient } = require("@supabase/supabase-js");
+
+const getKey = ({ directory, file }) => {
+  const path = file.path ? `${file.path}/` : "";
+  const fname = file.name.replace(/\.[^/.]+$/, "");
+
+  const filename = `${path}${fname}_${file.hash}${file.ext}`;
+
+  if (!directory) return filename;
+
+  return `${directory}/${filename}`.replace(/^\//g, "");
+};
+
+module.exports = {
+  provider: "supabase",
+  name: "Supabase Storage",
+  auth: {
+    apiUrl: {
+      label:
+        "Supabase API Url (e.g. 'https://zokyprbhquvvrleedkkk.supabase.co')",
+      type: "text",
+    },
+    apiKey: {
+      label:
+        "Supabase API Key (e.g. 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpBNWJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6NOYyNjk1NzgzOCwiZXhwIjoxOTQUNTMzODM4fQ.tfr1M8tg6-ynD-qXkODRXX-do1qWNwQQUt1zQp8sFIc')",
+      type: "text",
+    },
+    bucket: {
+      label: "Supabase storage bucket (e.g. 'my-bucket')",
+      type: "text",
+    },
+    directory: {
+      label: "Directory inside Supabase storage bucket (e.g. '')",
+      type: "text",
+    },
+    options: {
+      label: "Supabase client additional options",
+      type: "object",
+    },
+  },
+  init: (config) => {
+    const apiUrl = config.apiUrl;
+    const apiKey = config.apiKey;
+
+    const bucket = config.bucket || "strapi-uploads";
+    const options = config.options || undefined;
+    let directory = (config.directory || "").replace(/(^\/)|(\/$)/g, "");
+
+    const year = new Date().getFullYear().toString();
+    const month = (new Date().getMonth() + 1).toString();
+
+    if (!directory && options?.dynamic_directory) {
+      directory = `${year}/${month}`;
+    }
+
+    delete options?.dynamic_directory;
+
+    const supabase = createClient(apiUrl, apiKey, options);
+
+    const upload = (file) =>
+      new Promise((resolve, reject) => {
+        //--- Compute the file key.
+        file.hash = new Date().getTime();
+        //--- Upload the file into storage
+
+        const fileBuffer = getKey({ directory, file });
+
+        supabase.storage
+          .from(bucket)
+          .upload(
+            fileBuffer,
+            // file, // or Buffer.from(file.buffer, "binary"),
+            Buffer.from(file.buffer, "binary"), // or file
+            {
+              cacheControl: "public, max-age=31536000, immutable",
+              upsert: true,
+              contentType: file.mime,
+            }
+          )
+          .then(({ error }) => {
+            if (error) {
+              console.error(error);
+              return reject(error);
+            }
+
+            const {
+              data: { publicUrl },
+            } = supabase.storage
+              .from(bucket)
+              .getPublicUrl(getKey({ directory, file }));
+
+            file.url = publicUrl;
+            delete file.buffer;
+            resolve();
+          });
+      });
+
+    return {
+      upload,
+      uploadStream: (file) =>
+        new Promise((resolve, reject) => {
+          const _buf = [];
+
+          file.stream.on("data", (chunk) => _buf.push(chunk));
+          file.stream.on("end", () => {
+            file.buffer = Buffer.concat(_buf);
+            resolve(upload(file));
+          });
+          file.stream.on("error", (err) => reject(err));
+        }),
+
+      delete: (file) =>
+        new Promise((resolve, reject) => {
+          //--- Delete the file fromstorage the space
+          supabase.storage
+            .from(bucket)
+            .remove([getKey({ directory, file })])
+            .then(({ data, error }) => {
+              if (error) return reject(error);
+              resolve();
+            });
+        }),
+    };
+  },
+};
